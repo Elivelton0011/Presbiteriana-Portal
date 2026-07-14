17286dfee51794444360d2209a4b0f398d919d53
Elivelton Chaves <elicomercial0011@gmail.com>
2026-07-13 21:26:34 -0300

    feat(hero): cria seção hero com navegação e carrossel de 3 slides

---
+++ scripts/hero.js
@@ -0,0 +1,42 @@
+let list = document.querySelectorAll('.item')
+let next = document.getElementById('next')
+let prev = document.getElementById('prev')
+
+let count = list.length
+let active = 0
+
+
+function nextSlide() {
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    active = active >= count - 1 ? 0 : active + 1
+
+    list[active].classList.add('active')
+}
+
+
+function prevSlide() {
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    active = active <= 0 ? count - 1 : active - 1
+
+    list[active].classList.add('active')
+}
+
+
+next.onclick = () => {
+    nextSlide()
+}
+
+
+prev.onclick = () => {
+    prevSlide()
+}
+
+
+// troca automaticamente a cada 5 segundos
+setInterval(() => {
+    nextSlide()
+}, 100000)
